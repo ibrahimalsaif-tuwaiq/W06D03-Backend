b042e2f7b677164fd6409ad5711596e7bf57f20e
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-16 22:02:57 +0300

    getTodos controller

--- routers/controllers/todo.js
+++ routers/controllers/todo.js
@@ -1,18 +1,37 @@
+const fs = require("fs");
+
 const getTodos = (req, res) => {
-    // getTodos
-  };
-  
-  const addTodo = (req, res) => {
-    // addTodo
-  };
-  
-  const updateTodo = (req, res) => {
-    // updateTodo
-  };
-  
-  const deleteTodo = (req, res) => {
-    // deleteTodo
-  };
-  
-  module.exports = {getTodos, addTodo, updateTodo, deleteTodo};
-  
+  const userObject = req.body;
+
+  fs.readFile("./db/usersTodo.json", (err, data) => {
+    const users = JSON.parse(data.toString());
+
+    const loggedinUser = users.find(
+      (user) => user.username == userObject.username
+    );
+
+    if (loggedinUser) {
+      users.forEach((user) => {
+        if (user.username == loggedinUser.username) {
+          res.status(200).json(user.todos);
+        }
+      });
+    } else {
+      res.status(401).json("You have to login first!!");
+    }
+  });
+};
+
+const addTodo = (req, res) => {
+  // addTodo
+};
+
+const updateTodo = (req, res) => {
+  // updateTodo
+};
+
+const deleteTodo = (req, res) => {
+  // deleteTodo
+};
+
+module.exports = { getTodos, addTodo, updateTodo, deleteTodo };
--- routers/routes/todo.js
+++ routers/routes/todo.js
@@ -4,7 +4,7 @@ const {getTodos, addTodo, updateTodo, deleteTodo} = require("../controllers/todo
 
 const todoRouter = express.Router();
 
-todoRouter.get("/getAll", getTodos);
+todoRouter.post("/getAll", getTodos);
 todoRouter.post("/add", addTodo);
 todoRouter.put("/update", updateTodo);
 todoRouter.delete("/delete", deleteTodo);
