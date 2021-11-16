0859ecb8a29d39629ab90d0af2553fef33937a41
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-16 21:03:14 +0300

    signup controller

--- routers/controllers/signup.js
+++ routers/controllers/signup.js
@@ -1,5 +1,27 @@
+const fs = require("fs");
+
 const signup = (req, res) => {
-  // signup
+  const newUser = req.body.newUser;
+
+  fs.readFile("./db/usersTodo.json", (err, data) => {
+    const users = JSON.parse(data.toString());
+
+    users.forEach((user) => {
+      if (user.username == newUser.username && user.email == newUser.email) {
+        res.status(403).json("Username or Email already exists");
+      } else {
+        users.push({
+          username: newUser.username,
+          email: newUser.email,
+          password: newUser.password,
+          todos: [],
+        });
+        fs.writeFile("./db/usersTodo.json", JSON.stringify(users), () => {
+          res.status(200).json("User added");
+        });
+      }
+    });
+  });
 };
 
 module.exports = signup;
